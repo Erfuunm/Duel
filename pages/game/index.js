a4713391cf1afe142d24199b9a884041867d134f
Erfan <erfuun.mi82@gmail.com>
2024-11-18 09:35:52 +0330

    improved game.js component 1) add new score board component

--- pages/game/index.js
+++ pages/game/index.js
@@ -1,5 +1,5 @@
 //  data
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 import {
@@ -56,7 +56,7 @@ export const aboutData = [
         icons: [
           <SiBlazor key={2} />,
           <FaReact key={2} />,
-          
+
           <SiRedux key={2} />,
           <SiFramer key={2} />,
           <SiNextdotjs key={2} />,
@@ -126,92 +126,116 @@ export const aboutData = [
   },
 ];
 
-import Avatar from "../../components/Avatar";
+
 import Circles from '../../components/Circles';
 
 
-import { motion } from 'framer-motion';
-import { fadeIn } from '../../variants'
-import { icons } from 'react-icons';
 
-import CountUp from 'react-countup';
+
+/* From Uiverse.io by sofiapetushok */
+
+
+// function ScoreBoard() {
+
+
+//   return (
+//     <div className="card">
+//       <div data-status="inprogress" className="teams">
+//         {teams.map((team, index) => (
+//           <div key={index} className="team">
+//             <span className="team-info team-home">
+//               <span className="team-info-container">
+//                 <span className="team-name-info">{`${team.team1} - ${team.team2}`}</span>
+//               </span>
+//             </span>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+
+function ScoreBoard({ }) {
+
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    // This code runs only on the client side
+    const gameData = JSON.parse(localStorage.getItem('gameData'));
+    const playerNames = gameData ? gameData.playerNames : [];
+
+
+    const newTeams = [];
+
+    // Create teams from player names
+    for (let i = 0; i < playerNames.length / 2; i++) {
+      const PlayerA = playerNames[i];
+      const PlayerB = playerNames[playerNames.length - 1 - i];
+      newTeams.push({ PlayerA, PlayerB });
+    }
+
+    setTeams(newTeams);
+    console.log(newTeams)
+  }, []);
+  return (
+    <>
+      {
+        teams ?
+          <div class="card" >
+            <div data-status="inprogress" class="teams">
+              <span class="team-info team-home">
+                <span class="team-info-container">
+                  <span class="team-name-info">{teams[0]?.PlayerA + ' - ' + teams[0]?.PlayerB}</span>
+                </span>
+              </span>
+              <span class="event-scoreboard">
+                <span class="event-score-container">
+                  <span class="current-time-container">
+                    <span class="progress-dots" data-progress="1S">
+                      <span class="load"></span>
+                    </span>
+                  </span>
+                  <span class="score-container">
+                    <span class="score-home">2</span>
+                    <span class="custom-sep">-</span>
+                    <span class="score-away">4</span>
+                  </span>
+                </span>
+              </span>
+              <span class="team-info team-away">
+                <span class="team-info-container">
+                  <span class="team-icon-container"></span>
+                  <span class="team-name-info">{teams[1]?.PlayerA + ' - ' + teams[1]?.PlayerB}</span>
+                </span>
+              </span>
+            </div>
+          </div> :
+          null
+      }
+    </>
+  );
+}
 
 const Game = () => {
   const [index, setIndex] = useState(0);
   console.log(index);
   return (
     <div className='h-[800px] bg-primary/30  text-center xl:text-left  '>
+      <div className='mt-0 px-1'>
+      <ScoreBoard />
+      </div>
+     
       <Circles />
       <div className=' container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6 '>
         <div className='  mt-[150px] mb-[50px] xl:mt-[20px] flex-1 flex flex-col justify-center'>
-          <motion.h2 variants={fadeIn('right', 0.2)} initial='hidden' animate='show' exit='hidden' className=' h2'>
-            The Story Of <span className=' text-accent'>My Life</span>  how it started as Developer.
-          </motion.h2>
-          <motion.p variants={fadeIn('right', 0.4)} initial='hidden' animate='show' exit='hidden' className='  max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0 '>
-            4 years ago, I began freelancing as a Developer. Since then, i have
-            done remote work for agencies, counsulted for startups, and
-            collabrated on digital products for business and consumer use.
-          </motion.p>
-          <motion.div className=' hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 -mb-[100px]  '>
-            <div className='flex flex-1 xl:gap-x-6'>
-
-              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0 '></div>
-              <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
-                <CountUp start={0} end={4} duration={5} /> +
-
-              </div>
-              <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Years Of Experience</div>
-
-
-              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0 '></div>
-              <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
-                <CountUp start={0} end={25} duration={5} /> +
-
-              </div>
-              <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Projects</div>
 
 
 
-              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0 '></div>
-              <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
-                <CountUp start={0} end={10} duration={5} /> +
-
-              </div>
-              <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Language & FrameWorks</div>
 
 
-            </div>
-
-          </motion.div>
         </div>
-        <motion.div variants={fadeIn('left', 0.4)} initial='hidden' animate='show' exit='hidden' className='flex flex-col items-center w-full  -mt-12 xl:max-w-[48%] h-[480px] '>
-          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4 lg:mt-[150px]  '>
-            {aboutData.map((item, itemIndex) => {
-              return <div key={itemIndex} className={` ${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'}cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-                onClick={() => setIndex(itemIndex)}>{item.title}</div>;
-            })}
-          </div>
-          <div className=' xl:py-10 flex flex-col place-items-center gap-y-2 xl:gap-y-4 
-        '>
-            {aboutData[index].info.map((item, itemIndex) => {
-              return (
-                <div key={itemIndex} className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60 '>
-                  <div className='font-light mb-2 mr-2 md:mb-0'>{item.title}</div>
-                  <div className=' hidden md:flex'></div>
-                  <div>{item.stage}</div>
-                  <div className=' flex gap-x-4 '>
 
-                    {item.icons?.map((icon, itemIndex) => {
-                      return <div className='text-2xl text-white' key={itemIndex}>{icon}</div>
-                    })}
-
-
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </motion.div>
 
 
 
--- pages/index.js
+++ pages/index.js
@@ -1,109 +1,130 @@
 import ParticlesContainer from '../components/ParticlesContainer';
-
-import { Button, Divider } from '@nextui-org/react';
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Checkbox, Input, Link } from "@nextui-org/react";
-import { Fa4, Fa6, Fa8, Fa1, Fa0 } from "react-icons/fa6";
+import { Button } from '@nextui-org/react';
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@nextui-org/react";
+import { Fa4, Fa6, Fa8 } from "react-icons/fa6";
 import { BsFillPersonPlusFill } from "react-icons/bs";
-import { GiThrownKnife } from "react-icons/gi";
-import { GiDwarfFace } from "react-icons/gi";
 import { GiCrossedSwords } from "react-icons/gi";
 import { useState } from 'react';
+import { useRouter } from 'next/router';
+import toast, { Toaster } from 'react-hot-toast';
+import Nav from '../components/Nav';
+import Header from '../components/Header';
 
-
-function NameInput({ lable }) {
-
+// Name input component
+function NameInput({ label, value, onChange }) {
   return (
-    <>
-      <div class="flex flex-row gap-1 w-1/4 bg-white/80 rounded-md py-1 px-1">
-
-        <input id="" placeholder={lable} name="" type="text" class="bg-inherit w-full focus:outline-none pl-1" />
-      </div>
-
-    </>
-  )
+    <div className="flex flex-row gap-1 w-1/4 bg-white/80 rounded-md py-1 px-1">
+      <input
+        placeholder={label}
+        type="text"
+        className="bg-inherit w-full focus:outline-none pl-1"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
 }
 
-function FourPlayer({ }) {
-
+// Four player component
+function FourPlayer({ playerNames, playerChangeHandlers }) {
   return (
-    <>
-      <div className='flex flex-col items-center justify-center gap-8'>
-        <NameInput lable={'Player A'} />
-        <div className='flex flex-row justify-between items-center'>
-          <NameInput lable={'Player B'} />
-          <GiCrossedSwords size={50} className="icon" />
-          <NameInput lable={'Player B'} />
-        </div>
-        <NameInput lable={'Player A'} />
+    <div className='flex flex-col items-center justify-center gap-8'>
+      <NameInput label={'Player A'} value={playerNames[0]} onChange={playerChangeHandlers[0]} />
+      <div className='flex flex-row justify-between items-center'>
+        <NameInput label={'Player B'} value={playerNames[1]} onChange={playerChangeHandlers[1]} />
+        <GiCrossedSwords size={50} className="icon" />
+        <NameInput label={'Player C'} value={playerNames[2]} onChange={playerChangeHandlers[2]} />
       </div>
-
-    </>
-  )
+      <NameInput label={'Player D'} value={playerNames[3]} onChange={playerChangeHandlers[3]} />
+    </div>
+  );
 }
 
-function SixPlayer({ }) {
-
+// Six player component
+function SixPlayer({ playerNames, playerChangeHandlers }) {
   return (
-    <>
-      <div className='flex flex-col items-center justify-center gap-8'>
-        <div className='flex flex-row justify-around -space-x-16 items-center '>
-          <NameInput lable={'Player A'} />
-
-          <NameInput lable={'Player B'} />
-        </div>
-        <div className='flex flex-row justify-between items-center'>
-          <NameInput lable={'Player C'} />
-          <GiCrossedSwords size={50} className="icon" />
-          <NameInput lable={'Player C'} />
-        </div>
-        <div className='flex flex-row justify-around -space-x-16 items-center '>
-          <NameInput lable={'Player B'} />
-
-          <NameInput lable={'Player A'} />
-        </div>
+    <div className='flex flex-col items-center justify-center gap-8'>
+      <div className='flex flex-row justify-around -space-x-16 items-center'>
+        <NameInput label={'Player A'} value={playerNames[0]} onChange={playerChangeHandlers[0]} />
+        <NameInput label={'Player B'} value={playerNames[1]} onChange={playerChangeHandlers[1]} />
       </div>
-
-    </>
-  )
+      <div className='flex flex-row justify-between items-center'>
+        <NameInput label={'Player C'} value={playerNames[2]} onChange={playerChangeHandlers[2]} />
+        <GiCrossedSwords size={50} className="icon" />
+        <NameInput label={'Player D'} value={playerNames[3]} onChange={playerChangeHandlers[3]} />
+      </div>
+      <div className='flex flex-row justify-around -space-x-16 items-center'>
+        <NameInput label={'Player E'} value={playerNames[4]} onChange={playerChangeHandlers[4]} />
+        <NameInput label={'Player F'} value={playerNames[5]} onChange={playerChangeHandlers[5]} />
+      </div>
+    </div>
+  );
 }
 
-function EightPlayer({ }) {
-
+// Eight player component
+function EightPlayer({ playerNames, playerChangeHandlers }) {
   return (
-    <>
-      <div className='flex flex-col items-center justify-center gap-8'>
-        <div className='flex flex-row justify-between items-center '>
-          <NameInput lable={'Player A'} />
-          <NameInput lable={'Player D'} />
-          <NameInput lable={'Player B'} />
-        </div>
-        <div className='flex flex-row justify-between items-center'>
-          <NameInput lable={'Player C'} />
-          <GiCrossedSwords size={50} className="icon" />
-          <NameInput lable={'Player C'} />
-        </div>
-        <div className='flex flex-row justify-between items-center '>
-          <NameInput lable={'Player B'} />
-          <NameInput lable={'Player D'} />
-          <NameInput lable={'Player A'} />
-        </div>
+    <div className='flex flex-col items-center justify-center gap-8'>
+      <div className='flex flex-row justify-between items-center'>
+        <NameInput label={'Player A'} value={playerNames[0]} onChange={playerChangeHandlers[0]} />
+        <NameInput label={'Player B'} value={playerNames[1]} onChange={playerChangeHandlers[1]} />
+        <NameInput label={'Player C'} value={playerNames[2]} onChange={playerChangeHandlers[2]} />
       </div>
+      <div className='flex flex-row justify-between items-center'>
 
-    </>
-  )
+        <NameInput label={'Player D'} value={playerNames[3]} onChange={playerChangeHandlers[3]} />
+        <GiCrossedSwords size={50} className="icon" />
+        <NameInput label={'Player E'} value={playerNames[4]} onChange={playerChangeHandlers[4]} />
+      </div>
+      <div className='flex flex-row justify-between items-center'>
+
+        <NameInput label={'Player F'} value={playerNames[5]} onChange={playerChangeHandlers[5]} />
+        <NameInput label={'Player G'} value={playerNames[6]} onChange={playerChangeHandlers[6]} />
+        <NameInput label={'Player H'} value={playerNames[7]} onChange={playerChangeHandlers[7]} />
+      </div>
+    </div>
+  );
 }
-
-
 
 function PlayGame({ isOpen, onOpenChange }) {
+  const [mode, setMode] = useState(4);
+  const [playerNames, setPlayerNames] = useState(['', '', '', '']);
+  const router = useRouter();
 
-  const [mode , setMode] = useState(4);
+  const modeHandler = (newMode) => {
+    setMode(newMode);
+    if (newMode === 4) {
+      setPlayerNames(['', '', '', '']);
+    } else if (newMode === 6) {
+      setPlayerNames(['', '', '', '', '', '']);
+    } else if (newMode === 8) {
+      setPlayerNames(['', '', '', '', '', '', '', '']);
+    }
+  };
 
-  const modeHandler = (mode) => {
+  const onClose = () => {
+    const dataToStore = { mode, playerNames };
+    localStorage.setItem('gameData', JSON.stringify(dataToStore));
+    onOpenChange();
+  };
 
-    setMode(mode)
+  const playerChangeHandlers = playerNames.map((_, index) => (e) => {
+    const newNames = [...playerNames];
+    newNames[index] = e.target.value;
+    setPlayerNames(newNames);
+  });
 
-  }
+  const allPlayersFilled = playerNames.every(name => name.trim() !== '');
+
+  const handlePlay = () => {
+    if (!allPlayersFilled) {
+      toast.error('Please fill in all player names.')
+      return;
+    }
+
+    onClose(); // Optionally call onClose if you want to close the modal first
+    router.push('/game'); // Navigate to the game's page
+  };
 
   return (
     <Modal
@@ -114,124 +135,80 @@ function PlayGame({ isOpen, onOpenChange }) {
       className='bg-gradient-to-tr from-transparent rounded-3xl via-[rgba(120,87,87,1.6)] to-transparent'
     >
       <ModalContent>
-        {(onClose) => (
-          <>
-            <ModalHeader className="flex flex-col gap-1">Game Settings</ModalHeader>
-            <hr className='bg-green-800 w-11/12 mx-auto' />
-            <ModalBody>
-
-              <div className='flex flex-row text-gray-900 items-center gap-2 '>
-                <BsFillPersonPlusFill size={25} />
-                <h3 className=''>Players :</h3>
-              </div>
-
-              <div className='circle-container flex justify-center items-center mt-10 mb-16'>
-
-                {
-                  mode === 4 ?
-                  <FourPlayer />
-                  : mode === 6 ?
-                  <SixPlayer /> :
-                  <EightPlayer />
-                }
-
-              </div>
-
-              <div className='flex flex-row gap-6 justify-center mb-8'>
-                <Button onClick={() => modeHandler(4)} isIconOnly className={` rounded-full border ${mode === 4 ? 'bg-slate-400 bg-opacity-35' : null} border-success`} aria-label="Like">
-                  <div className={`${mode === 4 ? 'text-gray-200' : 'text-gray-900'}`}>
-                    <Fa4 />
-                  </div>
-                </Button>
-                <Button  onClick={() => modeHandler(6)} isIconOnly className={` rounded-full border ${mode === 6 ? 'bg-slate-400 bg-opacity-35' : null} border-success`} aria-label="Like">
-                <div className={`${mode === 6 ? 'text-gray-200' : 'text-gray-900'}`}>
-                    <Fa6 />
-                  </div>
-                </Button>
-                <Button  onClick={() => modeHandler(8)} isIconOnly className={` rounded-full border ${mode === 8 ? 'bg-slate-400 bg-opacity-35' : null} border-success`} aria-label="Like">
-                <div className={`${mode === 8 ? 'text-gray-200' : 'text-gray-900'}`}>
-                    <Fa8 />
-                  </div>
-                </Button>
-              
-              </div>
-
-
-
-
-              {/* <div className="flex py-2 px-1 justify-between">
-                <Checkbox
-                  classNames={{
-                    label: "text-small",
-                  }}
-                >
-                  Remember me
-                </Checkbox>
-
-              </div> */}
-            </ModalBody>
-            <ModalFooter>
-              <Button color="accent" variant="flat" onPress={onClose}>
-                Close
+        <>
+          <ModalHeader className="flex flex-col gap-1">Game Settings</ModalHeader>
+          <hr className='bg-green-800 w-11/12 mx-auto' />
+          <ModalBody>
+            <div className='flex flex-row text-gray-900 items-center gap-2 '>
+              <BsFillPersonPlusFill size={25} />
+              <h3>Players :</h3>
+            </div>
+            <div className='circle-container flex justify-center items-center mt-10 mb-16'>
+              {mode === 4 && <FourPlayer playerNames={playerNames} playerChangeHandlers={playerChangeHandlers} />}
+              {mode === 6 && <SixPlayer playerNames={playerNames} playerChangeHandlers={playerChangeHandlers} />}
+              {mode === 8 && <EightPlayer playerNames={playerNames} playerChangeHandlers={playerChangeHandlers} />}
+            </div>
+            <div className='flex flex-row gap-6 justify-center mb-8'>
+              <Button onClick={() => modeHandler(4)} isIconOnly className={`rounded-full border ${mode === 4 ? 'bg-slate-400 bg-opacity-35' : ''} border-success`} aria-label="4 Players">
+                <Fa4 className={`${mode === 4 ? 'text-gray-200' : 'text-gray-900'}`} />
               </Button>
-              <Button className=' rounded-xl' color="secondary" onPress={onClose}>
-                Play
+              <Button onClick={() => modeHandler(6)} isIconOnly className={`rounded-full border ${mode === 6 ? 'bg-slate-400 bg-opacity-35' : ''} border-success`} aria-label="6 Players">
+                <Fa6 className={`${mode === 6 ? 'text-gray-200' : 'text-gray-900'}`} />
               </Button>
-            </ModalFooter>
-          </>
-        )}
+              <Button onClick={() => modeHandler(8)} isIconOnly className={`rounded-full border ${mode === 8 ? 'bg-slate-400 bg-opacity-35' : ''} border-success`} aria-label="8 Players">
+                <Fa8 className={`${mode === 8 ? 'text-gray-200' : 'text-gray-900'}`} />
+              </Button>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="accent" variant="flat" onPress={onClose}>
+              Close
+            </Button>
+            <Button onClick={handlePlay} className='rounded-xl' color="secondary">
+              Play
+            </Button>
+          </ModalFooter>
+        </>
       </ModalContent>
     </Modal>
-
   );
 }
 
-
 const Home = () => {
-
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
-    <div className='bg-primary/60 h-full relative'> {/* Set 'relative' here */}
-      <div className='w-full h-full -mt-0  bg-gradient-to-r from-primary/10 via-black/30 to-black/10 '>
-        <div className='text-center flex flex-col justify-center xl:pt-40  xl:text-left h-full container mx-auto'>
-          {/* Uncomment the title and paragraph if needed */}
-          {/* <motion.h1 variants={fadeIn('down' , 0.2)} initial='hidden' animate='show' exit='hidden' className='h1  '> Time For Migration <br /> Into <span className=' text-blue-500'>Digital Reality :)</span></motion.h1>
-          <motion.p variants={fadeIn('down' , 0.3)} initial='hidden' animate='show' exit='hidden' className='max-w-sm xl:max-w-xl pb-[50px] mx-auto xl:mx-0 mb-10 xl:mb-16'>
-          </motion.p > */}
-
-          <div className='flex flex-col gap-8 w-3/4 mx-auto '>
+    <div className='bg-primary/60 h-full relative'>
+      <Toaster />
+      <Header />
+      <div className='w-full h-full -mt-0 bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+          <div className='flex flex-col gap-8 w-3/4 mx-auto'>
             <button onClick={onOpen}
-              className="text-zinc-300 hover:text-green-600 backdrop-blur-lg bg-gradient-to-tr from-transparent via-[rgba(95,87,87,0.36)] to-transparent rounded-md py-2 px-6 shadow hover:shadow-green-600 duration-700 z-10" // Added z-index
+              className="text-zinc-300 hover:text-green-600 backdrop-blur-lg bg-gradient-to-tr from-transparent via-[rgba(95,87,87,0.36)] to-transparent rounded-md py-2 px-6 shadow hover:shadow-green-600 duration-700 z-10"
             >
               Play Game
             </button>
             <button
-              className="text-zinc-300 hover:text-green-600 backdrop-blur-lg bg-gradient-to-tr from-transparent via-[rgba(95,87,87,0.36)] to-transparent rounded-md py-2 px-6 shadow hover:shadow-green-600 duration-700 z-10" // Added z-index
+              className="text-zinc-300 hover:text-green-600 backdrop-blur-lg bg-gradient-to-tr from-transparent via-[rgba(95,87,87,0.36)] to-transparent rounded-md py-2 px-6 shadow hover:shadow-green-600 duration-700 z-10"
             >
               Settings
             </button>
             <button
-              className="text-zinc-300 hover:text-green-600 backdrop-blur-lg bg-gradient-to-tr from-transparent via-[rgba(95,87,87,0.36)] to-transparent rounded-md py-2 px-6 shadow hover:shadow-green-600 duration-700 z-10" // Added z-index
+              className="text-zinc-300 hover:text-green-600 backdrop-blur-lg bg-gradient-to-tr from-transparent via-[rgba(95,87,87,0.36)] to-transparent rounded-md py-2 px-6 shadow hover:shadow-green-600 duration-700 z-10"
             >
               Exit
             </button>
-
-
             <div className='z-10 bg-red-700'>
-
               <PlayGame isOpen={isOpen} onOpenChange={onOpenChange} />
             </div>
-
           </div>
         </div>
       </div>
-
-
-
-      <div className='w-full h-full absolute right-0 bottom-0 pointer-events-none'> {/* Add 'pointer-events-none' to disable interaction */}
+      <div className='w-full h-full absolute right-0 bottom-0 pointer-events-none'>
         <div className='bg-none xl:bg-explosion animate-pulse xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
         </div>
+        <Nav />
         <ParticlesContainer />
       </div>
     </div>
