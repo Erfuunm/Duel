10c71073966ff8b905699432d0e8d4106c9598e7
Erfan <erfuun.mi82@gmail.com>
2024-11-19 19:34:49 +0330

    add Done and Failed button for game.js

--- pages/game/index.js
+++ pages/game/index.js
@@ -129,7 +129,32 @@ export const aboutData = [
 
 import Circles from '../../components/Circles';
 import Image from 'next/image';
+import { Avatar, Button } from '@nextui-org/react';
 
+function PlayerCard({ teams }) {
+  return (
+
+    <div className="card2">
+      <div className="content">
+        <div className="back">
+          <div className="back-content">
+            {
+              teams ?
+                <div className='flex items-center justify-start ml-10 w-full gap-6'>
+                  <Avatar isBordered color="default" />
+                  <span className=' text-2xl pb-2 border-b-2 border-zinc-800 '>{teams[0]?.PlayerA}</span>
+                </div> :
+                null
+            }
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  );
+}
 
 
 
@@ -139,15 +164,31 @@ function Card() {
   return (
 
     <div class="book bg-primary/30">
-       
+
       <img src="" />
       <div class="cover">
-      <Image src={'/logo-smal.png'} width={100} height={88} alt='' priority={true}/>
+        <Image src={'/logo-smal.png'} width={100} height={88} alt='' priority={true} />
       </div>
     </div>
 
   );
 }
+
+
+function MyButton({ lable, color }) {
+  return (
+
+    <button class="button">
+      <span class={`button_lg ${color}` }>
+        <span class="button_sl"></span>
+        <span class="button_text">{lable}</span>
+      </span>
+    </button>
+
+  );
+}
+
+
 
 
 function ScoreBoread2({ teams, scores }) {
@@ -187,7 +228,23 @@ function ScoreBoread2({ teams, scores }) {
 }
 
 
-function ScoreBoard({ }) {
+function ScoreBoard({ teams, scores }) {
+
+
+  return (
+    <>
+      {
+        teams ?
+          <ScoreBoread2 teams={teams} scores={scores} />
+          :
+          null
+      }
+    </>
+  );
+}
+
+const Game = () => {
+  const [index, setIndex] = useState(0);
 
   const [teams, setTeams] = useState([]);
   const [scores, setScores] = useState(['0', '0', '0', '0'])
@@ -210,33 +267,33 @@ function ScoreBoard({ }) {
     setTeams(newTeams);
     console.log(newTeams)
   }, []);
-  return (
-    <>
-      {
-        teams ?
-          <ScoreBoread2 teams={teams} scores={scores} />
-          :
-          null
-      }
-    </>
-  );
-}
 
-const Game = () => {
-  const [index, setIndex] = useState(0);
   console.log(index);
   return (
     <div className='h-full bg-primary/30  text-center xl:text-left  '>
-  
+
       <div className='mt-0 px-1'>
-        <ScoreBoard />
+        <ScoreBoard scores={scores} teams={teams} />
       </div>
       <Circles />
-      <div className=' container mx-auto h-full flex justify-center items-center xl:flex-row gap-x-6 '>
-        <div className='  -mt-[35%]  flex flex-col justify-center'>
+      <div className=' container mx-auto h-full flex flex-col justify-center items-center  gap-x-6 '>
+        <div className='  -mt-[30%]  flex flex-col justify-center gap-16'>
 
+          <div className='flex justify-center items-center'>
+            <PlayerCard teams={teams} />
+          </div>
           <Card />
+          <div className='flex justify-center items-center'>
+            <PlayerCard teams={teams} />
+          </div>
 
+
+
+        </div>
+        <div className='flex items-center mt-16 gap-9'>
+
+          <MyButton lable={'Failed'}  color={'bg-red-600'} />
+          <MyButton lable={'Done'} color={'bg-green-600'} />
 
 
         </div>
