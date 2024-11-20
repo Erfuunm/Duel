5320879d715bdd5f12d20d5b6ad0101d545317fc
Erfan <erfuun.mi82@gmail.com>
2024-11-20 11:38:03 +0330

    improved game.js 1) add random function for select game

--- pages/game/index.js
+++ pages/game/index.js
@@ -6,6 +6,10 @@ import Image from 'next/image';
 import { Avatar, Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from '@nextui-org/react';
 import { FaRegHandScissors } from "react-icons/fa";
 import { RiInformationLine } from "react-icons/ri";
+import { PiHandGrabbingThin } from "react-icons/pi";
+import { FaSnapchat } from "react-icons/fa6";
+import { FaStreetView } from "react-icons/fa6";
+import { PiPerson } from "react-icons/pi";
 
 function PlayerCard({ playerName }) {
   return (
@@ -28,7 +32,74 @@ function PlayerCard({ playerName }) {
   );
 }
 
-function HintModal({ isOpen, onOpenChange }) {
+
+function RSPGame() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  return (
+    <>
+      <Button isIconOnly className='fixed top-4 left-4 ' onPress={onOpen}><RiInformationLine size={40} className='text-yellow-200' /></Button>
+      <div className='flex flex-col items-center justify-center gap-8'>
+        <Avatar isBordered className='w-16 h-16' icon={< FaRegHandScissors className='text-gray-300 ' size={35} />} />
+        <HintModal text={'in this game you should chose one player (not from your team) and play Paper - Scissors - Rock.If you beat her then your team will catch the point'} isOpen={isOpen} onOpenChange={onOpenChange} />
+        <span className='text-white text-xl'>Paper - Scissors - Rock</span>
+      </div>
+    </>
+  );
+}
+
+function WichHand() {
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  return (
+    <>
+      <Button isIconOnly className='fixed top-4 left-4 ' onPress={onOpen}><RiInformationLine size={40} className='text-yellow-200' /></Button>
+      <div className='flex flex-col items-center justify-center gap-8'>
+        <Avatar isBordered className='w-16 h-16' icon={< PiHandGrabbingThin className='text-gray-300 ' size={35} />} />
+        <HintModal text={'in this game you should chose one player (not from your team) and play Guees Wich Hand. If you beat her then your team will catch the point'} isOpen={isOpen} onOpenChange={onOpenChange} />
+        <span className='text-white text-xl'>Guess Wich Hand</span>
+      </div>
+    </>
+  );
+}
+
+function Dower() {
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  return (
+    <>
+      <Button isIconOnly className='fixed top-4 left-4 ' onPress={onOpen}><RiInformationLine size={40} className='text-yellow-200' /></Button>
+      <div className='flex flex-col items-center justify-center gap-2'>
+        <div className='flex flex-col items-center gap-3'>
+        <Avatar isBordered className='w-16 h-16' icon={< FaSnapchat className='text-gray-300 ' size={35} />} />
+        <span className='text-yellow-200 text-lg'>Dowr : </span>
+        </div>
+        <HintModal text={'in this game your team mate have to find the word . do not talk about the word directly'} isOpen={isOpen} onOpenChange={onOpenChange} />
+        <span className='text-white text-xl '>"نیروگاه هسته ای توس"</span>
+      </div>
+    </>
+  );
+}
+
+
+function Panto() {
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  return (
+    <>
+      <Button isIconOnly className='fixed top-4 left-4 ' onPress={onOpen}><RiInformationLine size={40} className='text-yellow-200' /></Button>
+      <div className='flex flex-col items-center justify-center gap-2'>
+        <div className='flex flex-col items-center gap-3'>
+        <Avatar isBordered className='w-16 h-16' icon={< PiPerson className='text-gray-300 ' size={35} />} />
+        <span className='text-yellow-200 text-lg'>Pantomime : </span>
+        </div>
+        <HintModal text={'in this game your team mate have to find the word . do not talk about the word directly'} isOpen={isOpen} onOpenChange={onOpenChange} />
+        <span className='text-white text-xl '>"از منظراین بادیه"</span>
+      </div>
+    </>
+  );
+}
+
+function HintModal({ text, isOpen, onOpenChange }) {
   return (
     <Modal
       isOpen={isOpen}
@@ -44,8 +115,8 @@ function HintModal({ isOpen, onOpenChange }) {
           <ModalBody>
             <div className='flex flex-row text-gray-300 items-center gap-2  '>
 
-              <h4 className='mt-2 mb-2'>in this game you should chose one player (not from your team) and play Paper - Scissors - Rock.
-                If you beat her then your team will catch the point .
+              <h4 className='mt-2 mb-2'>
+                {text}
               </h4>
             </div>
 
@@ -61,51 +132,33 @@ function HintModal({ isOpen, onOpenChange }) {
 
 /* From Uiverse.io by sofiapetushok */
 
-function Card() {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+function Card({ currentComponent }) {
   return (
-
-    <div class="book bg-primary/30">
-      <Button isIconOnly className='fixed top-4 left-4 ' onPress={onOpen}><RiInformationLine size={40} className='text-yellow-200' /></Button>
-      <div className='flex flex-col items-center justify-center gap-8'>
-
-        <Avatar isBordered className='w-16 h-16' icon={< FaRegHandScissors className='text-gray-300 ' size={35} />} />
-        <HintModal isOpen={isOpen} onOpenChange={onOpenChange} />
-        <span className='text-white text-xl'>Paper - Scissors - Rock</span>
-
-      </div>
-      <div class="cover">
+    <div className="book bg-primary/30">
+      {currentComponent}
+      <div className="cover">
         <Image src={'/logo-smal.png'} width={100} height={88} alt='' priority={true} />
       </div>
     </div>
-
   );
 }
 
-function MyButton({ Mode, lable, color, round, setRound }) {
 
-  const changeRound = () => {
-    console.log('sajajjajaja')
+function MyButton({ Mode, label, color, round, setRound, changeComponent }) {
+  const handleClick = () => {
     if (Mode === 4) {
-
-      if (round === 0) {
-        setRound(1)
-      } else if (round === 1) {
-        setRound(0)
-      }
-
+      setRound((prevRound) => (prevRound === 0 ? 1 : 0));
     }
-  }
+    changeComponent(); // Change component on button click
+  };
 
   return (
-
-    <button onClick={() => changeRound()} class="button">
-      <span class={`button_lg ${color}`}>
+    <button onClick={handleClick} className="button">
+      <span className={`button_lg ${color}`}>
         <span class="button_sl"></span>
-        <span class="button_text">{lable}</span>
+        <span class="button_text">{label}</span>
       </span>
     </button>
-
   );
 }
 
@@ -166,25 +219,27 @@ function ScoreBoard({ teams, scores }) {
 
 const Game = () => {
   const [index, setIndex] = useState(0);
-
-  const [teams, setTeams] = useState([]); //storing teams name
-  const [scores, setScores] = useState(['0', '0', '0', '0']) //storing teams scores
+  const [teams, setTeams] = useState([]);
+  const [scores, setScores] = useState(['0', '0', '0', '0']);
   const [round, setRound] = useState(1);
   const [mode, setMode] = useState();
+  const [currentComponent, setCurrentComponent] = useState(null);
+
+  const components = [<Panto />, <Dower />, <WichHand />, <RSPGame />];
+
+  const getRandomComponent = () => {
+    const randomIndex = Math.floor(Math.random() * components.length);
+    return components[randomIndex];
+  };
 
   useEffect(() => {
-    // This code runs only on the client side
+    // Retrieve game data from localStorage
     const gameData = JSON.parse(localStorage.getItem('gameData'));
-    console.log(gameData)
     const playerNames = gameData ? gameData.playerNames : [];
-
-    setMode(gameData ? gameData.mode : null)
-
-
-
-    const newTeams = [];
+    setMode(gameData ? gameData.mode : null);
 
     // Create teams from player names
+    const newTeams = [];
     for (let i = 0; i < playerNames.length / 2; i++) {
       const PlayerA = playerNames[i];
       const PlayerB = playerNames[playerNames.length - 1 - i];
@@ -192,37 +247,33 @@ const Game = () => {
     }
 
     setTeams(newTeams);
-    console.log(newTeams)
+    setCurrentComponent(getRandomComponent()); // Initialize with a random component
   }, []);
 
   console.log(index);
-  return (
-    <div className='h-full bg-primary/30  text-center xl:text-left  '>
+  const changeComponent = () => {
+    setCurrentComponent(getRandomComponent());
+  };
 
+  return (
+    <div className='h-full bg-primary/30 text-center xl:text-left'>
       <div className='mt-0 px-1'>
         <ScoreBoard scores={scores} teams={teams} />
       </div>
       <Circles />
-      <div className=' container mx-auto h-full flex flex-col justify-center items-center  gap-x-6 '>
-        <div className='  -mt-[30%]  flex flex-col justify-center gap-16'>
-
+      <div className='container mx-auto h-full flex flex-col justify-center items-center gap-x-6'>
+        <div className='-mt-[30%] flex flex-col justify-center gap-16'>
           <div className='flex justify-center items-center'>
             <PlayerCard playerName={teams[round]?.PlayerA} />
           </div>
-          <Card />
+          <Card currentComponent={currentComponent} />
           <div className='flex justify-center items-center'>
             <PlayerCard playerName={teams[round]?.PlayerB} />
           </div>
-
-
-
         </div>
         <div className='flex items-center mt-16 gap-9'>
-
-          <MyButton Mode={mode} lable={'Failed'} color={'bg-red-600'} round={round} setRound={setRound} />
-          <MyButton Mode={mode} lable={'Done'} color={'bg-green-600'} round={round} setRound={setRound} />
-
-
+          <MyButton Mode={mode} label={'Failed'} color={'bg-red-600'} round={round} setRound={setRound} changeComponent={changeComponent} />
+          <MyButton Mode={mode} label={'Done'} color={'bg-green-600'} round={round} setRound={setRound} changeComponent={changeComponent} />
         </div>
       </div>
     </div>
